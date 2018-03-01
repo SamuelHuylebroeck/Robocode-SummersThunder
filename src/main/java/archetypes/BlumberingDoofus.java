ee828ad16132f712c789b4366e3cc8208f734e5b
Samuel Huylebroeck <samuel.huylebroeck@hotmail.com>
2018-03-01 18:31:15 +0100

    BlumberingDoofus implementation

---
+++ src/main/java/archetypes/BlumberingDoofus.java
@@ -0,0 +1,7 @@
+package archetypes;
+
+/**
+ * Created by Samuel on 28/02/2018.
+ */
+public class BlumberingDoofus {
+}
---
+++ src/main/java/robots/SummersThunderBasic.java
@@ -0,0 +1,7 @@
+package robots;
+
+/**
+ * Created by Samuel on 28/02/2018.
+ */
+public class SummersThunder {
+}
